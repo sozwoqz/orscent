52c13fa048d809f5e1e53191341d9c55e3b190a0
sozwoqz <zenasni295@gmail.com>
2026-05-06 20:13:50 +0200

    fix images

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
       { protocol: 'https', hostname: 'cdn.shopify.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
     ],
+    domains: ['images.unsplash.com', 'cdn.shopify.com'],
   },
 }
 
